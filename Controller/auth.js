94bdebf67e05ab2a7a44b29047f180f6a4d229b2
niraj <nirajversilal@gmail.com>
2025-02-14 16:01:46 +0500

    test

--- Controller/auth.js
+++ Controller/auth.js
@@ -5,25 +5,25 @@ export const CreateUser = async (req, res, next) => {
   const { token } = req.body;
   try {
     const decode = jwt.decode(token);
-    switch (decode.type) {
-      case "user.created":
-        const { id, email, first_name, last_name } = decode.data.user;
-        await User.create({
-          kindeID: id,
-          email,
-          first_name,
-          last_name,
-        });
-        res.status(201).json({ message: "User created successfully" });
-        break;
-      case "user.deleted":
-        const { id: deleteUserID } = decode.data.user;
-        await User.deleteOne({ kindeID: deleteUserID });
-        res.status(201).json({ message: "User delete successfully" });
-        break;
-      default:
-        return;
-    }
+    // switch (decode.type) {
+    //   case "user.created":
+    //     const { id, email, first_name, last_name } = decode.data.user;
+    //     await User.create({
+    //       kindeID: id,
+    //       email,
+    //       first_name,
+    //       last_name,
+    //     });
+    //     res.status(201).json({ message: "User created successfully" });
+    //     break;
+    //   case "user.deleted":
+    //     const { id: deleteUserID } = decode.data.user;
+    //     await User.deleteOne({ kindeID: deleteUserID });
+    //     res.status(201).json({ message: "User delete successfully" });
+    //     break;
+    //   default:
+    //     return;
+    // }
     return res.status(200).json({ message: decode });
   } catch (error) {
     next(error);
